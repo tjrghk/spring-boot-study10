1502fdf37e63581dc12626b7bed5d6384e4acd6a
tjrghk <tjrghk1992@gmail.com>
2022-08-19 16:36:20 +0900

    쿠키, 세션 처리

---
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,38 @@
+package hello.login.web.session;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import hello.login.domain.member.Member;
+
+public class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionsTest() {
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+       
+        //세션 생성
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        //요청에 응답 쿠키 저장
+        MockHttpServletRequest request =  new MockHttpServletRequest();
+        request.setCookies(response.getCookies()); 
+       
+        //세션 조회
+        Object result = sessionManager.getSession(request);
+        Assertions.assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        Assertions.assertThat(expired).isNull();
+    }
+}
